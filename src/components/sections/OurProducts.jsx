e0fd9520b94fdcc761f96e82f8d87ccd4103f0fc
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-04-10 09:21:05 -0300

    fix: Zera o texto da OurProducts

--- src/components/sections/OurProducts.jsx
+++ src/components/sections/OurProducts.jsx
@@ -15,55 +15,55 @@ export default function OurProducts() {
     <div className="flex justify-center full bg-quinary" id="products">
       <div className="w-[88%] max-w-[1110px] py-[26px] phone3:py-[48px]">
         <SectionHeader
-          sectionHeaderTitleSecond="Produtos e Acessórios"
-          sectionHeaderDescription="Aqui você encontra as melhores marcas. Escolha aquela da sua confiança e boas compras!"
+          sectionHeaderTitleSecond="A definir."
+          sectionHeaderDescription="A definir."
         />
         <SectionContent>
           <ImgAndButtonsCard
             img={hthLogo}
-            button1Label="Consultar produtos"
+            button1Label="A definir."
             button1Link="/hth"
             alt="Logo da marca Hth"
           />
           <ImgAndButtonsCard
             img={gencoLogo}
-            button1Label="Consultar produtos"
+            button1Label="A definir."
             button1Link="/genco"
             alt="Logo da marca Genco"
           />
           <ImgAndButtonsCard
             img={neoclorLogo}
-            button1Label="Consultar produtos"
+            button1Label="A definir."
             button1Link="/neoclor"
             alt="Logo da marca Neoclor"
           />
           <ImgAndButtonsCard
             img={hidroazulLogo}
-            button1Label="Consultar produtos"
+            button1Label="A definir."
             button1Link="/hidroazul"
             alt="Logo da marca Hidroazul"
           />
           <ImgAndButtonsCard
             img={domclorLogo}
-            button1Label="Consultar produtos"
+            button1Label="A definir."
             button1Link="/domclor"
             alt="Logo da marca Domclor"
           />
           <ImgAndButtonsCard
             img={suallLogo}
-            button1Label="Consultar produtos"
+            button1Label="A definir."
             button1Link="/suall"
             alt="Logo da marca Suall"
           />
           <ImgAndButtonsCard
             img={sodramarLogo}
-            button1Label="Consultar acessórios"
+            button1Label="A definir."
             button1Link="/sodramar"
             alt="Logo da marca Sodramar"
           />
           <ImgAndButtonsCard
             img={netunoLogo}
-            button1Label="Consultar acessórios"
+            button1Label="A definir."
             button1Link="/netuno"
             alt="Logo da marca Netuno"
           />
