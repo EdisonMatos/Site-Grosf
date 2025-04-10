367a7b67d973db3f103e131e690e3d96d1ac9daa
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-04-10 09:38:57 -0300

    fix: Zera conteúdo do Faq

--- src/components/sections/FrenquentlyAskedQuestions.jsx
+++ src/components/sections/FrenquentlyAskedQuestions.jsx
@@ -1,7 +1,7 @@
 import MotionDivRightToLeft from "../animation/MotionDivRightToLeft";
 import SectionHeader from "../sectionElements/SectionHeader";
 import Acordion from "../interactives/Acordion";
-import faqImg from "../../assets/imgs/faq/faq.webp";
+import faqImg from "../../assets/imgs/faq/faq.jpg";
 
 export default function FrequentlyAskedQuestions() {
   return (
