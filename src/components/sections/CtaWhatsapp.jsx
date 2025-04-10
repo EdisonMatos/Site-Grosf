93bd9265ad19448ac10321df7116bb3e8281ac90
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-04-10 09:28:22 -0300

    fix: Zera conteúdo do CtaWhatsapp

--- src/components/sections/CtaWhatsapp.jsx
+++ src/components/sections/CtaWhatsapp.jsx
@@ -1,5 +1,5 @@
 import SectionContent from "../sectionElements/SectionContent";
-import imgCta from "../../assets/importAssets/CtaWhatsapp.webp";
+import imgCta from "../../assets/importAssets/CtaWhatsapp.jpg";
 import imgBackground from "../../assets/importAssets/bg2.webp";
 import WhatsAppIcon from "../../assets/importAssets/WhatsAppIcon.webp";
 import MotionDivUpToDown from "../animation/MotionDivUpToDown";
@@ -27,27 +27,29 @@ export default function CtaWhatsapp() {
             <div className="flex flex-col w-full  justify-evenly items-left h-28 phone1:h-40 phone3:h-48 desktop1:h-52 desktop1:mb-[24px]">
               <h1 className="text-left text-white text-paragraph1 phone1:text-paragraph4 phone3:text-paragraph5 max-w-32 phone3:max-w-none font-poppins tablet1:text-title2 tablet2:text-title4 desktop1:text-title5 desktop2:text-title6">
                 <MotionDivUpToDown>
-                  <p>Contate-nos através do <strong>Whatsapp</strong></p>
+                  <p>
+                    A definir <strong>A definir.</strong>
+                  </p>
                 </MotionDivUpToDown>
               </h1>
               <MotionDivDownToUp>
                 <div className="w-full">
                   <button className="w-full">
-                  <a
-                    href={whatsappContactLink}
-                    className="flex flex-row items-center justify-around w-[90%] h-[32px] phone1:h-[48px] px-[8px] phone3:px-[16px] tablet1:px-[20px] transition rounded-[10px] phone3:rounded-[30px] tablet1:rounded-[40px] max-w-[128px] tablet1:h-[64px] phone3:max-w-[192px] tablet1:max-w-[380px] phone3:w-[90%] bg-red-700 hover:bg-red-900"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <img
-                      src={WhatsAppIcon}
-                      className="h-[45%] phone3:h-[50%] tablet1:h-[32px]"
-                      alt="WhatsApp Icon"
-                    ></img>
-                    <p className="text-white text-center text-paragraph1 phone3:text-paragraph2 phone3:pr-[5px] tablet1:pr-[3%] tablet1:text-title1">
-                      Fale com um consultor
-                    </p>
-                  </a>
+                    <a
+                      href={whatsappContactLink}
+                      className="flex flex-row items-center justify-around w-[90%] h-[32px] phone1:h-[48px] px-[8px] phone3:px-[16px] tablet1:px-[20px] transition rounded-[10px] phone3:rounded-[30px] tablet1:rounded-[40px] max-w-[128px] tablet1:h-[64px] phone3:max-w-[192px] tablet1:max-w-[380px] phone3:w-[90%] bg-red-700 hover:bg-red-900"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <img
+                        src={WhatsAppIcon}
+                        className="h-[45%] phone3:h-[50%] tablet1:h-[32px]"
+                        alt="WhatsApp Icon"
+                      ></img>
+                      <p className="text-white text-center text-paragraph1 phone3:text-paragraph2 phone3:pr-[5px] tablet1:pr-[3%] tablet1:text-title1">
+                        A definir.
+                      </p>
+                    </a>
                   </button>
                 </div>
               </MotionDivDownToUp>
