7bccc7692ddfefeed12405c4d4a8d41e9a0aa8ed
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-04-10 09:30:00 -0300

    fix: Zera conteúdo do Courses

--- src/components/sections/Courses.jsx
+++ src/components/sections/Courses.jsx
@@ -10,29 +10,29 @@ export default function Courses() {
     <div className="flex justify-center full bg-quinary" id="courses">
       <div className="w-[88%] max-w-[1110px] py-[26px] phone3:py-[48px]">
         <SectionHeader
-          sectionHeaderTitleSecond="Aprimore seu conhecimento"
-          sectionHeaderDescription="Confira abaixo nossas sugestões para palestras ou cursos para tratamento da sua piscina. Escolha o que mais lhe interessar, e boas aulas!"
+          sectionHeaderTitleSecond="A definir."
+          sectionHeaderDescription="A definir."
         />
         <SectionContent>
           <ImgTextAndButtonsCard
             img={hthLogo}
-            text="Curso presencial de tratamento de águas de piscinas."
-            button1Label="Faça sua inscrição"
+            text="A definir."
+            button1Label="A definir."
             button1Link="https://hth.com.br/cursos-presenciais
             "
             alt="Logo da marca Hth"
           />
           <ImgTextAndButtonsCard
             img={gencoLogo}
-            text="Curso online na Escola da Piscina."
-            button1Label="Faça sua inscrição"
+            text="A definir."
+            button1Label="A definir."
             button1Link="https://www.genco.com.br/escola-da-piscina/"
             alt="Logo da marca Genco"
           />
           <ImgTextAndButtonsCard
             img={hidroazulLogo}
-            text="Curso completo na Universidade da Piscina."
-            button1Label="Faça sua inscrição"
+            text="A definir."
+            button1Label="A definir."
             button1Link="https://hidroazul.com.br/universidade-da-piscina/"
             alt="Logo da marca Hidroazul"
           />
