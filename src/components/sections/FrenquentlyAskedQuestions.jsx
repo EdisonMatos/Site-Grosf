31d6062e1243166c929e6dcb388d2862cc30dd3e
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-04-11 12:11:24 -0300

    fix: Imagem Faq

--- src/components/sections/FrenquentlyAskedQuestions.jsx
+++ src/components/sections/FrenquentlyAskedQuestions.jsx
@@ -1,7 +1,7 @@
 import MotionDivRightToLeft from "../animation/MotionDivRightToLeft";
 import SectionHeader from "../sectionElements/SectionHeader";
 import Acordion from "../interactives/Acordion";
-import faqImg from "../../assets/imgs/faq/faq.jpg";
+import faqImg from "../../assets/imgs/faq/faq.png";
 
 export default function FrequentlyAskedQuestions() {
   return (
