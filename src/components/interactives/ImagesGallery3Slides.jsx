862ac04c2ffd8efa54d05e4d9ef12ac77090df5e
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-06-16 11:18:53 -0300

    fix: Remove comentário

--- src/components/interactives/ImagesGallery3Slides.jsx
+++ src/components/interactives/ImagesGallery3Slides.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ImageGallery from "react-image-gallery";
 import PropTypes from "prop-types";
 import "react-image-gallery/styles/css/image-gallery.css";
-// sobe codigo
 
 export default function ImagesGallery3Slides(props) {
   const {
