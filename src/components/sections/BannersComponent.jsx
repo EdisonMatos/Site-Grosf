92973fba313027a95ea23c6421b3d7d19a5fbb40
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-07-23 17:23:06 -0300

    fix: Seção ajustada

--- src/components/sections/BannersComponent.jsx
+++ src/components/sections/BannersComponent.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
+import SectionHeader from "../sectionElements/SectionHeader";
 
 // Importe as imagens corretamente
 import banner0 from "../../assets/banners/bannersCarousel/banners0.png";
@@ -60,55 +61,54 @@ export default function BannerCarouselComponent() {
 
   return (
     <section className="w-full bg-quinary py-12">
-      <div className="text-center mb-8">
-        <h2 className="text-3xl md:text-4xl font-bold text-primary">
-          a_Definir
-        </h2>
-        <p className="text-gray-600 mt-2">a_Definir</p>
-      </div>
-
-      <div className="relative w-[90%] max-w-screen-xl mx-auto overflow-hidden rounded-2xl shadow-lg h-64 md:h-[550px]">
-        {banners.map((banner, index) => (
-          <div
-            key={banner.id}
-            className={`absolute inset-0 transition-opacity duration-1000 ease-in-out ${
-              index === current ? "opacity-100 z-10" : "opacity-0 z-0"
-            }`}
-          >
-            <img
-              src={banner.image}
-              alt={banner.title}
-              className="w-full h-full object-cover"
-            />
-            <div className="absolute inset-0 bg-black bg-opacity-30 flex flex-col justify-center items-center text-white p-4"></div>
-          </div>
-        ))}
-
-        {/* Botões de navegação */}
-        <button
-          onClick={prevSlide}
-          className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-primary bg-opacity-100 text-white p-2 rounded-full hover:bg-opacity-70 z-20"
-        >
-          <ChevronLeft />
-        </button>
-
-        <button
-          onClick={nextSlide}
-          className="absolute top-1/2 right-4 transform -translate-y-1/2 bg-primary bg-opacity-100 text-white p-2 rounded-full hover:bg-opacity-70 z-20"
-        >
-          <ChevronRight />
-        </button>
-
-        {/* Indicadores */}
-        <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex gap-2 z-20">
-          {banners.map((_, index) => (
+      <div className="w-[90%] max-w-[1110px] m-auto">
+        <SectionHeader
+          sectionHeaderTitleFirst=" 5 passos simples para cuidar da sua piscina"
+          sectionHeaderDescription="Mantenha a água limpa, segura e cristalina com cuidados práticos no dia a dia."
+        />
+        <div className="relative w-[90%] max-w-screen-xl mx-auto overflow-hidden rounded-2xl shadow-lg h-64 md:h-[550px]">
+          {banners.map((banner, index) => (
             <div
-              key={index}
-              className={`w-3 h-3 rounded-full transition-all duration-300 ${
-                index === current ? "bg-primary scale-110" : "bg-white"
+              key={banner.id}
+              className={`absolute inset-0 transition-opacity duration-1000 ease-in-out ${
+                index === current ? "opacity-100 z-10" : "opacity-0 z-0"
               }`}
-            ></div>
+            >
+              <img
+                src={banner.image}
+                alt={banner.title}
+                className="w-full h-full object-cover"
+              />
+              <div className="absolute inset-0 bg-black bg-opacity-30 flex flex-col justify-center items-center text-white p-4"></div>
+            </div>
           ))}
+
+          {/* Botões de navegação */}
+          <button
+            onClick={prevSlide}
+            className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-primary bg-opacity-100 text-white p-2 rounded-full hover:bg-opacity-70 z-20"
+          >
+            <ChevronLeft />
+          </button>
+
+          <button
+            onClick={nextSlide}
+            className="absolute top-1/2 right-4 transform -translate-y-1/2 bg-primary bg-opacity-100 text-white p-2 rounded-full hover:bg-opacity-70 z-20"
+          >
+            <ChevronRight />
+          </button>
+
+          {/* Indicadores */}
+          <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex gap-2 z-20">
+            {banners.map((_, index) => (
+              <div
+                key={index}
+                className={`w-3 h-3 rounded-full transition-all duration-300 ${
+                  index === current ? "bg-primary scale-110" : "bg-white"
+                }`}
+              ></div>
+            ))}
+          </div>
         </div>
       </div>
     </section>
