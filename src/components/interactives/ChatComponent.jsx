26c21f2a397bbb4f72a1e4facb1019332c258294
Gabriel Souza <gabrielsouza65115@gmail.com>
2025-12-29 07:14:57 -0300

    fix: Atualiza versão

--- src/components/interactives/ChatComponent.jsx
+++ src/components/interactives/ChatComponent.jsx
@@ -283,7 +283,7 @@ export default function CalculaFacilGrosf() {
         ))}
       </div>
       <p className="font-mainFont flex justify-center pb-4 text-black">
-        Versão 4.0{' '}
+        Versão 5.0{' '}
       </p>
 
       {step === 'start' && (
