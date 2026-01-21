39963c94c4db8c2a8b2d05d2ddb0ed29b9742495
Gabriel Souza <gabrielsouza65115@gmail.com>
2026-01-21 09:54:51 -0300

    fix: Remove bg preto na frente do Banner

--- src/components/sections/BannersComponent.jsx
+++ src/components/sections/BannersComponent.jsx
@@ -147,7 +147,6 @@ export default function BannerCarouselComponent() {
                 alt={banner.title}
                 className="w-full h-full object-contain rounded-2xl"
               />
-              <div className="absolute inset-0 bg-black bg-opacity-30 flex flex-col justify-center items-center text-white p-4"></div>
             </div>
           ))}
 
@@ -193,7 +192,6 @@ export default function BannerCarouselComponent() {
                 alt={banner.title}
                 className="w-full h-full object-cover"
               />
-              <div className="absolute inset-0 bg-black bg-opacity-30 flex flex-col justify-center items-center text-white p-4"></div>
             </div>
           ))}
 
