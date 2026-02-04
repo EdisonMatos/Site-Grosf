5a5d7dd52250ec4d79461fb5884bf8d1c4ca600c
Gabriel Souza <gabrielsouza65115@gmail.com>
2026-02-04 14:48:53 -0300

    fix: Botoes

--- src/components/cards/ImgAndButtonsCard.jsx
+++ src/components/cards/ImgAndButtonsCard.jsx
@@ -1,21 +1,21 @@
-import PropTypes from "prop-types";
-import MotionDivLeftToRight from "../animation/MotionDivLeftToRight";
-import { Link } from "react-router-dom";
-import Buttons from "../interactives/Buttons";
+import PropTypes from 'prop-types'
+import MotionDivLeftToRight from '../animation/MotionDivLeftToRight'
+import { Link } from 'react-router-dom'
+import Buttons from '../interactives/Buttons'
 
 export default function ImgAndButtonsCard(props) {
-  const { img, alt, button1Label, button1Link } = props;
+  const { img, alt, button1Label, button1Link } = props
 
   const handleClick = () => {
-    window.location.href = button1Link + "#";
-  };
+    window.location.href = button1Link + '#'
+  }
 
   ImgAndButtonsCard.propTypes = {
     img: PropTypes.any,
     alt: PropTypes.string,
     button1Label: PropTypes.any,
     button1Link: PropTypes.any,
-  };
+  }
 
   return (
     <div className="w-full phone3:w-[45%] desktop1:w-[23%]  bg-white p-[15px] my-[14px] rounded-[10px] flex flex-col gap-[15px]">
@@ -29,8 +29,8 @@ export default function ImgAndButtonsCard(props) {
         {/* <button className="text-white bg-primary rounded-[10px] text-paragraph3 py-[8px] px-[31px] font-mainFont font-medium w-full transition hover:bg-blue-600">
           <MotionDivLeftToRight><p>{button1Label}</p></MotionDivLeftToRight>
         </button> */}
-        <Buttons className="w-full h-8 mb-4"/>
+        <Buttons className="w-full h-10 desktop1:h-8 mb-4" />
       </Link>
     </div>
-  );
+  )
 }
