e5a3a2e0d272897b5403eee44d132b7c070b65db
Gabriel Souza <gabrielsouza65115@gmail.com>
2026-04-11 10:01:20 -0300

    fix: Numero

--- src/abstractions/whats.js
+++ src/abstractions/whats.js
@@ -1,4 +1,4 @@
 const whatsappNumber =
-  "+55119989598507?text=Olá! Vim através do site de vocês e gostaria de tirar umas dúvidas.";
+  "+5511989598507?text=Olá! Vim através do site de vocês e gostaria de tirar umas dúvidas.";
 
 export default whatsappNumber;
